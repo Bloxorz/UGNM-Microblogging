88e7a788c876e3e67b28ac7f402bc3d41be62807
Marvin <marvin.neurath@rwth-aachen.de>
2014-11-05 14:47:42 +0100

    GSON Dependeny added

---
+++ QAServise/src/main/Project/DTO/AbstractDTO.java
@@ -0,0 +1,17 @@
+package Project.DTO;
+
+/**
+ * Created by Marv on 05.11.2014.
+ */
+public class AbstractDTO {
+
+    long id;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
