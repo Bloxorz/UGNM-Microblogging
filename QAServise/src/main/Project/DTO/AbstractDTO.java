6f00b10546c26bec3f392f8d5f2cd1c48547f140
Marvin <marvin.neurath@rwth-aachen.de>
2014-11-05 14:51:32 +0100

    AbstractDTO added

--- QAServise/src/main/Project/DTO/AbstractDTO.java
+++ QAServise/src/main/Project/DTO/AbstractDTO.java
@@ -3,14 +3,21 @@ package Project.DTO;
 /**
  * Created by Marv on 05.11.2014.
  */
-public class AbstractDTO {
+public class AbstractDTO implements IDTO {
 
-    long id;
+    private long id;
 
+    @Override
+    public void setId() {
+
+    }
+
+    @Override
     public long getId() {
         return id;
     }
 
+    @Override
     public void setId(long id) {
         this.id = id;
     }
