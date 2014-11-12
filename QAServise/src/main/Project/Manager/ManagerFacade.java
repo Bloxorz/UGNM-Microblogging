15668d0fccf6c531cc00c76b0464ca28be8cee9e
adabru <b.brunnmeier@gmx.de>
2014-11-12 18:11:44 +0100

    ManagerFacade fast fertig. Fehlt noch ExpertiseManager

--- QAServise/src/main/Project/Manager/ManagerFacade.java
+++ QAServise/src/main/Project/Manager/ManagerFacade.java
@@ -1,7 +1,10 @@
 package Project.Manager;
 
-import Project.DTO.HashtagDTO;
+import Project.DTO.*;
+import Project.Exceptions.*;
 
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,10 +17,22 @@ public class ManagerFacade {
 
     private static ManagerFacade instance;
 
-    private ManagerFacade() {}
+    private UserManager userManager;
+    private ExpertiseManager expertiseManager;
+    private HashtagManager hashtagManager;
+    private QuestionManager questionManager;
+    private AnswerManager answerManager;
+
+    private ManagerFacade() {
+        userManager = new UserManager();
+        expertiseManager = new ExpertiseManager();
+        hashtagManager = new HashtagManager();
+        questionManager = new QuestionManager();
+        answerManager = new AnswerManager();
+    }
 
     public static ManagerFacade getInstance() {
-        if(instance == null)
+        if (instance == null)
             ManagerFacade.instance = new ManagerFacade();
         return instance;
     }
@@ -27,4 +42,89 @@ public class ManagerFacade {
         //TODO
         return result;
     }
+
+    ////// userManager //////
+    public List<UserDTO> getUserList(Connection conn) throws SQLException {
+        return userManager.getUserList(conn);
+    }
+
+    public long addUser(Connection conn, UserDTO user) throws SQLException, CantInsertException {
+        return userManager.addUser(conn, user);
+    }
+
+    public UserDTO getUser(Connection conn, long userId) throws SQLException {
+        return userManager.getUser(conn, userId);
+    }
+
+    public void editUser(Connection conn, UserDTO user) throws SQLException, NotWellFormedException, CantUpdateException {
+        userManager.editUser(conn, user);
+    }
+
+    public void deleteUser(Connection conn, long userId) throws SQLException, CantUpdateException {
+        userManager.deleteUser(conn, userId);
+    }
+
+    public List<QuestionDTO> bookmarkedQuestions(Connection conn, long userId) throws SQLException {
+        return userManager.bookmarkedQuestions(conn, userId);
+    }
+
+    public void bookmark(Connection conn, long userId, long questionId) throws SQLException, CantInsertException {
+        userManager.bookmark(conn, userId, questionId);
+    }
+    ////// expertiseManager //////
+    ////// hashtagManager //////
+    public List<HashtagDTO> getHashtagCollection(Connection conn) throws SQLException {
+        return hashtagManager.getHashtagCollection(conn);
+    }
+    public HashtagDTO getHashtag(Connection conn, long hashtagId) throws SQLException {
+        return hashtagManager.getHashtag(conn, hashtagId);
+    }
+    public HashtagDTO addHashtag(Connection conn, String text) throws SQLException {
+        return hashtagManager.addHashtag(conn, text);
+    }
+    public void updateHashtag(Connection conn, HashtagDTO hashtag) throws SQLException {
+        hashtagManager.updateHashtag(conn, hashtag);
+    }
+    public void deleteHashtag(Connection conn, long hashtagId) throws SQLException {
+        hashtagManager.deleteHashtag(conn, hashtagId);
+    }
+    public List<HashtagDTO> getAllQuestionsToHashtag(Connection conn, long hashtagId) throws SQLException {
+        return hashtagManager.getAllQuestionsToHashtag(conn, hashtagId):
+    }
+    ////// questionManager //////
+    public List<QuestionDTO> getQuestionList(Connection conn) throws SQLException {
+        return questionManager.getQuestionList(conn);
+    }
+    public long addQuestion(Connection conn, QuestionDTO question) throws SQLException, CantInsertException {
+        return questionManager.addQuestion(conn, question);
+    }
+    public QuestionDTO getQuestion(Connection conn, long questionId) throws SQLException {
+        return questionManager.getQuestion(conn, questionId);
+    }
+    public void editQuestion(Connection conn, String questionText) throws SQLException, CantUpdateException {
+        questionManager.editQuestion(conn, questionText);
+    }
+    public void deleteQuestion(Connection conn, long questionId) throws SQLException, CantDeleteException {
+        questionManager.deleteQuestion(conn, questionId);
+    }
+    public List<AnswerDTO> getAnswersToQuestion(Connection conn, long questionId) throws SQLException {
+        return questionManager.getAnswersToQuestion(conn, questionId):
+    }
+    public long addAnswerToQuestion(Connection conn, AnswerDTO answer) throws SQLException, CantInsertException {
+        return questionManager.addAnswerToQuestion(conn, answer);
+    }
+    public List<UserDTO> getBookmarkUsersToQuestion(Connection conn, long questionId) throws SQLException {
+        return questionManager.getBookmarkUsersToQuestion(conn, questionId);
+    }
+    ////// answerManager //////
+    public AnswerDTO getAnswer(Connection conn, long answerId) throws SQLException {
+        return answerManager.getAnswer(conn, answerId);
+    }
+    public void editAnswer(Connection conn, long answerId, String answerText, int rating) throws SQLException, CantUpdateException {
+        answerManager.editAnswer(conn, answerId, answerText, rating);
+    }
+    public void deleteAnswer(Connection conn, long answerId) throws SQLException, CantDeleteException {
+        answerManager.deleteAnswer(conn, answerId);
+    }
 }
+
