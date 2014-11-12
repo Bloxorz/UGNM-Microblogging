d5002009db37f02098b0760534ad7132c92ebb02
Marvin <marvin.neurath@rwth-aachen.de>
2014-11-12 12:53:31 +0100

    userResource

--- QAServise/src/main/Project/Resources/UserResource.java
+++ QAServise/src/main/Project/Resources/UserResource.java
@@ -1,7 +1,23 @@
 package Project.Resources;
 
+import Project.DTO.UserDTO;
+import i5.las2peer.restMapper.HttpResponse;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
 /**
  * Created by Marv on 12.11.2014.
  */
 public class UserResource extends AbstractResource {
+
+    public HttpResponse getUserCollection(String token) {
+        throw new NotImplementedException();
+    }
+
+    public HttpResponse addUser(String token, UserDTO user) {
+        throw  new NotImplementedException();
+    }
+
+    public HttpResponse getUser(String token, long userId) {
+        throw  new NotImplementedException();
+    }
 }
