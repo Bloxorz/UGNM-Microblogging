f520cafaddeb820ef4ff77ef3f73ceb3d82e831e
Marvin <marvin.neurath@rwth-aachen.de>
2014-11-12 12:33:11 +0100

    example resource method

--- QAServise/src/main/Project/Manager/ManagerFacade.java
+++ QAServise/src/main/Project/Manager/ManagerFacade.java
@@ -1,5 +1,10 @@
 package Project.Manager;
 
+import Project.DTO.HashtagDTO;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Marv on 12.11.2014.
  */
@@ -15,5 +20,11 @@ public class ManagerFacade {
         if(instance == null)
             ManagerFacade.instance = new ManagerFacade();
         return instance;
+    }
+
+    public List<HashtagDTO> getHashtags() {
+        List<HashtagDTO> result = new ArrayList<HashtagDTO>();
+        //TODO
+        return result;
     }
 }
--- QAServise/src/main/Project/Resources/HashtagResource.java
+++ QAServise/src/main/Project/Resources/HashtagResource.java
@@ -1,9 +1,33 @@
 package Project.Resources;
 
+import Project.DTO.HashtagDTO;
+import Project.Manager.ManagerFacade;
+import com.google.gson.Gson;
+import i5.las2peer.restMapper.HttpResponse;
+
+import java.util.List;
+
 /**
  * Created by Marv on 12.11.2014.
  */
 public class HashtagResource extends AbstractResource {
 
+    //TODO in der serviceclass wrappen
+    public HttpResponse getHashtagCollection() {
+        List<HashtagDTO> allHashtags = ManagerFacade.getInstance().getHashtags();
+
+        //TODO
+        Gson gson = new Gson();
+        String json = "";
+        HttpResponse response = new HttpResponse(json);
+        //TODO
+
+        int status = 404;
+        response.setStatus(status);
+
+
+        return response;
+
+    }
 
 }
