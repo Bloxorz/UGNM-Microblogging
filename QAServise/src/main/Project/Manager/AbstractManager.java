715591a37f2d659451e2cb49255d5528cb1bfd66
Marvin <marvin.neurath@rwth-aachen.de>
2014-11-12 13:14:51 +0100

    abstract Manager

---
+++ QAServise/src/main/Project/Manager/AbstractManager.java
@@ -0,0 +1,7 @@
+package Project.Manager;
+
+/**
+ * Created by Marv on 12.11.2014.
+ */
+public abstract class AbstractManager {
+}
---
+++ QAServise/src/main/Project/Manager/QuestionManager.java
@@ -0,0 +1,7 @@
+package Project.Manager;
+
+/**
+ * Created by Marv on 12.11.2014.
+ */
+public class QuestionManager extends AbstractManager{
+}
