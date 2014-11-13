8604e54aba0356cf608d266ae73d4a7769f5e5c6
jk565463 <jan.kehren@rwth-aachen.de>
2014-11-13 01:38:07 +0100

    Chery-pick versuch

--- QAServise/src/main/i5/las2peer/services/servicePackage/Resources/UserResource.java
+++ QAServise/src/main/i5/las2peer/services/servicePackage/Resources/UserResource.java
@@ -1,15 +1,12 @@
 package i5.las2peer.services.servicePackage.Resources;
 
-import i5.las2peer.services.servicePackage.DTO.QuestionDTO;
 import i5.las2peer.services.servicePackage.DTO.UserDTO;
 import i5.las2peer.services.servicePackage.Exceptions.CantInsertException;
-<<<<<<< HEAD
-import i5.las2peer.services.servicePackage.Exceptions.CantUpdateException;
-=======
->>>>>>> dca1c9b... UserResource update
 import i5.las2peer.services.servicePackage.Exceptions.NotWellFormedException;
 import i5.las2peer.services.servicePackage.Manager.ManagerFacade;
 import i5.las2peer.restMapper.HttpResponse;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.List;
@@ -60,111 +57,32 @@ public class UserResource extends AbstractResource {
           catch (CantInsertException e){
         	  response.setStatus(304);
           }
-<<<<<<< HEAD
-
-=======
         
           catch (NotWellFormedException e){
         	  
         	  response.setStatus(400);
           }
         
->>>>>>> dca1c9b... UserResource update
         return response;
     }
 
     public HttpResponse getUser(String token, long userId) {
-    	
-    	HttpResponse response = new HttpResponse("");
-        try {
-            UserDTO user = ManagerFacade.getInstance().getUser(token, conn, userId);
-
-            Gson gson = new Gson();
-            String json = gson.toJson(user);
-            response = new HttpResponse(json);
-            response.setStatus(200);
-
-        } catch (SQLException e) {
-           response.setStatus(500);
-        }
-        return response;
+        throw  new NotImplementedException();
     }
-    
 
     public HttpResponse editUser(String token, UserDTO user) {
-    	
-    	HttpResponse response = new HttpResponse("");
-        try {
-            ManagerFacade.getInstance().editUser(token, conn, user);
-            response.setStatus(200);
-
-        } catch (SQLException e) {
-           response.setStatus(500);
-        }
-        
-          catch (CantUpdateException e){
-        	  response.setStatus(304);
-          }
-        
-          catch (NotWellFormedException e){
-        	  
-        	  response.setStatus(400);
-          }
-        
-        return response;
-    	
+        throw  new NotImplementedException();
     }
 
     public HttpResponse deleteUser(String token, long UserId) {
-    	
-    	HttpResponse response = new HttpResponse("");
-        try {
-            ManagerFacade.getInstance().deleteUser(token, conn, UserId);
-            response.setStatus(200);
-
-        } catch (SQLException e) {
-           response.setStatus(500);
-        }
-        
-          catch (CantUpdateException e){
-        	  response.setStatus(304);
-          }
-        
-        return response;
+        throw  new NotImplementedException();
     }
 
     public HttpResponse questionBookmarks(String token, long UserId) {
-    	
-    	HttpResponse response = new HttpResponse("");
-        try {
-            List<QuestionDTO> user = ManagerFacade.getInstance().bookmarkedQuestions(token, conn, UserId);
-
-            Gson gson = new Gson();
-            String json = gson.toJson(user);
-            response = new HttpResponse(json);
-            response.setStatus(200);
-
-        } catch (SQLException e) {
-           response.setStatus(500);
-        }
-        return response;
+        throw  new NotImplementedException();
     }
 
-    public HttpResponse addBookmark(String token, long UserId, long QuestionId) {
-    	
-    	HttpResponse response = new HttpResponse("");
-        try {
-            ManagerFacade.getInstance().bookmark(token, conn, UserId, QuestionId);
-            response.setStatus(200);
-
-        } catch (SQLException e) {
-           response.setStatus(500);
-        }
-        
-          catch (CantInsertException e){
-        	  response.setStatus(304);
-          }
-        
-        return response;
+    public HttpResponse addBookmark(String token, long QuestionId) {
+        throw  new NotImplementedException();
     }
 }
